7452c765f0a9d128941247d9c310a56c282864cf
Adit <aditsharma1203@gmail.com>
2022-09-11 10:09:06 +0530

    Completed the DiceGame code

--- DiceGame.c
+++ DiceGame.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-	printf("Hello world!!");
+	char name1[20], name2[20];
+	printf("\nPLAYER 1\n\tEnter name:\t");
+		scanf("\n%[^\n]s\n",name1);
+	printf("\nPLAYER 2\n\tEnter name:\t");
+		scanf("\n%[^\n]s\n",name2);
+
+	int score1 = 0, score2 = 0;
+
+	srand(time(NULL));
+	getchar();
+	for(int i=1; i<6; i++){
+		printf("\n----------------------------------------\n");
+		int x = rand() % 6 + 1;
+		int y = rand() % 6 + 1;
+		score1 += x;
+		score2 += y;
+		printf("Turn %d:\n",i);
+		printf("%s rolls the die...   	", name1);		for(int j=0; j<999999999; j++);	printf("%d\n",x);
+		// for(int j=0; j<999999999; j++);
+		printf("%s rolls the die...   	", name2);		for(int j=0; j<999999999; j++);	printf("%d\n",y);
+
+		printf("\nPRESS ENTER TO CONTINUE\n");	getchar();
+	}
+
+	printf("\n\n%s is the winner!!!!\n\n",(score1 > score2)?name1:name2);
 	return 0;
 }
